eeccd1c27a1b3157cf65e8c770185ec4e92182a2
Menghan <menghan412@gmail.com>
2013-04-07 21:48:46 +0800

    up

--- pycam2.py
+++ pycam2.py
@@ -11,12 +11,6 @@ def repeat():
     frame = cv.QueryFrame(capture)
     cv.ShowImage('w1', frame)
     c = cv.WaitKey(10)
-    if c == 'n':  # in 'n' key is pressed while the popup window is in focus
-        camera_index += 1  # try the next camera index
-        capture = cv.CaptureFromCAM(camera_index)
-        if not capture:  # if the next camera index didn't work, reset to 0.
-            camera_index = 0
-            capture = cv.CaptureFromCAM(camera_index)
 
 
 def main():
