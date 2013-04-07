8197ad9fbe0833d167c9f6246f738fb1c2695007
Menghan <menghan412@gmail.com>
2013-04-07 21:51:49 +0800

    up

--- pycam2.py
+++ pycam2.py
@@ -1,21 +1,14 @@
 import cv
 
-cv.NamedWindow('w1', cv.CV_WINDOW_AUTOSIZE)
-camera_index = 0
-capture = cv.CaptureFromCAM(camera_index)
-
-
-def repeat():
-    global capture  # declare as globals since we are assigning to them now
-    global camera_index
-    frame = cv.QueryFrame(capture)
-    cv.ShowImage('w1', frame)
-    c = cv.WaitKey(10)
-
 
 def main():
+    cv.NamedWindow('w1', cv.CV_WINDOW_AUTOSIZE)
+    camera_index = 0
+    capture = cv.CaptureFromCAM(camera_index)
     while True:
-        repeat()
+        frame = cv.QueryFrame(capture)
+        cv.ShowImage('w1', frame)
+        cv.WaitKey(10)
 
 
 if __name__ == '__main__':
