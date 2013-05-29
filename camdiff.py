5139ac528f96f9f8708a107f8e74de95bf139bcf
Menghan <menghan412@gmail.com>
2013-05-29 08:59:50 +0800

    better

--- camdiff.py
+++ camdiff.py
@@ -18,6 +18,7 @@ def get_gray_frame(capture):
 
 
 def get_frame_hist(frame):
+    cv.Smooth(frame, frame, cv.CV_GAUSSIAN)
     hist = cv.CreateHist([256], cv.CV_HIST_ARRAY, [[0, 255]], 1)
     cv.CalcHist([frame], hist)
     return hist
@@ -32,22 +33,34 @@ def main():
     interval = 1
     capture = cv.CaptureFromCAM(0)
     try:
+        last_frame = None
         last_hist = None
+        last_filename = None
+        last_saved = False
         while True:
             frame = get_gray_frame(capture)
+            filename = get_file_name()
             hist = get_frame_hist(frame)
             change = False
             if last_hist is not None:
                 method = cv.CV_COMP_CORREL
                 dist = cv.CompareHist(hist, last_hist, method)
-                change = dist <= 0.99
+                change = dist <= 0.995
+                # print filename, last_filename, change, dist
                 if change:
-                    cv.SaveImage(get_file_name(), frame)
+                    if not last_saved:
+                        cv.SaveImage(last_filename, last_frame)
+                    cv.SaveImage(filename, frame)
+                    last_saved = True
+                else:
+                    last_saved = False
             last_hist = hist
+            last_frame = frame
+            last_filename = filename
             if change:
-                interval = 0.75
+                interval = 0.4
             else:
-                interval = 1
+                interval = 0.8
             time.sleep(interval)
     except KeyboardInterrupt:
         pass
